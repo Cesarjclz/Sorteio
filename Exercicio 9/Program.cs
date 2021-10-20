b117c86d2580b1e4ad62f9a8e9f4b85fa5793a57
César Aguiar <Cesarphz21@gmail.com>
2021-10-20 12:36:43 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var e = new Random().Next(1, 5);
+
+            switch (e) {
+                case 1: Console.WriteLine("Você ganhou um PS4");
+                    break;
+                case 2: Console.WriteLine("Você ganhou um xbox");
+                    break;
+                case 3: Console.WriteLine("Você ganhou um celular");
+                    break;
+                case 4: Console.WriteLine("Você ganhou um PC");
+                    break;
+
+
+            
+            
+            }
+            
+            
+
+
+
+
+        }
+    }
+}
